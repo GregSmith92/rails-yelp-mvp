5acd9ba8ab534308d2032252e2dc8f63ead875b7
Greg Smith <greg_smith1992@hotmail.com>
2019-05-13 13:49:52 +0100

    Added CRUD methods for the restaurant model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/restaurants", to: "restaurants#index"
+  post "restaurants", to: "restaurants#create"
+  get "/restaurants/new", to: "restaurants#new"
+  get "/restaurants/:id/edit", to: "restaurants#edit", as: 'restaurant_edit'
+  get "/restaurants/:id", to: "restaurants#show", as: 'restaurant'
+  patch "/restaurants/:id", to: "restaurants#update"
+  delete "restaurants/:id", to: "restaurant#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    category:      'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    category:      'italian'
+  },
+  {
+    name:         'Pizza Union',
+    address:      '7 Pizza Street, London E5 7TE',
+    phone_number:  'A pizza union age decor.',
+    category:      'italian'
+  },
+  {
+    name:         'Falaffel Place',
+    address:      'Hoxton Market',
+    phone_number:  'A market stool with good falafel',
+    category:      'belgian'
+  },
+  {
+    name:         'Itsu',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    category:       'japanese'
+  },
+  {
+    name:         'La Poulet',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    category:      'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
