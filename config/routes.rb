006be943e93fcb1f7cff211f66ad179f92b6dd81
Greg Smith <greg_smith1992@hotmail.com>
2019-05-13 15:13:55 +0100

    Included MVC for reviews so that users can leave, and view reviews for each restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get "/restaurants", to: "restaurants#index"
-  post "restaurants", to: "restaurants#create"
-  get "/restaurants/new", to: "restaurants#new"
-  get "/restaurants/:id/edit", to: "restaurants#edit", as: 'restaurant_edit'
-  get "/restaurants/:id", to: "restaurants#show", as: 'restaurant'
-  patch "/restaurants/:id", to: "restaurants#update"
-  delete "restaurants/:id", to: "restaurant#destroy"
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews, only: [:show]
 end
+  # get "/restaurants", to: "restaurants#index"
+  # post "restaurants", to: "restaurants#create"
+  # get "/restaurants/new", to: "restaurants#new"
+  # get "/restaurants/:id/edit", to: "restaurants#edit", as: 'restaurant_edit'
+  # get "/restaurants/:id", to: "restaurants#show", as: 'restaurant'
+  # patch "/restaurants/:id", to: "restaurants#update"
+  # delete "restaurants/:id", to: "restaurant#destroy" do
